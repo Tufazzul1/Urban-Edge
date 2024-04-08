42b56d0cec7f8072945747b2ff1f34c64eac3b65
Tufazzul1 <tufajjul2017@gmail.com>
2024-04-08 23:38:07 +0545

    project setup done

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA72YcHZTsEN4DCJlqI5skj7M9TMQE2Xfg",
+  authDomain: "urban-edge-1327d.firebaseapp.com",
+  projectId: "urban-edge-1327d",
+  storageBucket: "urban-edge-1327d.appspot.com",
+  messagingSenderId: "538327079883",
+  appId: "1:538327079883:web:ac5dd327132e78136b744e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
